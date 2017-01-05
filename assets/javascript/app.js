a5c5e15372a46cf54a1a212d10577471416f014e
James <jwin4740@gmail.com>
2017-01-05 12:34:47 -0500

    cat

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,47 @@
 $(document).ready(function() {
-            var questionDiv = "";
+    var questionDiv = "";
+    var scienceArray = [];
 
-            
 
+    // prototype object
+    function question(category, prompt, choiceA, choiceB, choiceC, choiceD) {
+        this.category = category;
+        this.prompt = prompt;
+        this.choiceA = choiceA;
+        this.choiceB = choiceB;
+        this.choiceC = choiceC;
+        this.choiceD = choiceD;
 
-            // function that creates the question then inserts it into the DOM
+    };
 
-            // function generateQuestions() {
-            //     function makeForm() {
-            //         for (var i = 1; i <= 2; i++) {
-            //             questionDiv = $("#questions");
-            //             var newForm = $("<form>");
-            //             newForm.attr({id: "question" + i, class: "domanda"});
-            //             questionDiv.append(newForm);
+    // make Character objects for each player
+    var questionOne = new question("science", "what does H2O stand for?", "methane", "water", "carbon", "octane");
+    console.log(questionOne.prompt);
+    scienceArray.push(questionOne);
 
-            //             for (var j = 1; j < 5; j++) {
-            //                 var newDiv = $("<div class='choice" + j + " pick'>");
-            //                 $("#question" + i).append(newDiv);
-            //             }
-
-            //         }
-
-            //     }
-            //     makeForm();
-
-            //     function addChoices () {
-            //     	  for (var k = 1; k < 5; k++) {
-            //         var newInput = $("<input>");
-            //         newInput.attr({ type: "radio", name: "question", class: "option", data: String.fromCharCode(k + 64) });
-            //         console.log(String.fromCharCode(k + 64));
-            //         $(".choice" + k).append(newInput);
-            //     }
-
-            //     }
-            // 	addChoices();
-            // }
-            //     generateQuestions();
-              
-                $("#questions").on("click", ".domanda.choice.option", function() {
-                    var answer = $(this).data("value");
-                    console.log(answer);
-                });
+    var questionTwo = new question("science", "what does C6h6 stand for?", "methane", "hexane", "benzene", "octane");
+    console.log(questionTwo.prompt);
+    scienceArray.push(questionTwo);
+    console.log(scienceArray[0]);
 
 
 
+
+    function fillQuestionBox(x) {
+        $("#domanda").append(scienceArray[x].prompt);
+        $("#labelA").append(scienceArray[x].choiceA);
+        $("#labelB").append(scienceArray[x].choiceB);
+        $("#labelC").append(scienceArray[x].choiceC);
+        $("#labelD").append(scienceArray[x].choiceD);
+
+           }
+
+    fillQuestionBox(0);
+
+    $("#questions").on("click", ".domanda.choice.option", function() {
+        var answer = $(this).data("value");
+        console.log(answer);
+    });
 
 
 
@@ -54,4 +52,7 @@ $(document).ready(function() {
 
 
 
-            });
+
+
+
+});
