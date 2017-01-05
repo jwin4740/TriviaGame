b30487c4cc057a6e0a89dc9749298d3bc02d1508
James <jwin4740@gmail.com>
2017-01-05 03:12:37 -0500

    bed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,22 +7,23 @@ $(document).ready(function() {
     // function that creates the question then inserts it into the DOM
 
     function generateQuestions() {
-        for (var i = 1; i <= 2; i++) {
-            questionDiv = $("#questionsDom" + i);
+
+        for (var i = 1; i <= 1; i++) {
+            questionDiv = $("#questionsDom");
             var newForm = $("<form>");
             newForm.attr("id", "question" + i);
-            questionDiv.append(newForm).append("<hr style='clear: both;'>");
+            questionDiv.append(newForm);
             for (var j = 1; j < 5; j++) {
                 var newDiv = $("<div class='choice" + j + " pick'>");
                 $("#question" + i).append(newDiv);
             }
-            for (var k = 1; k <= 5; k++) {
+            for (var k = 1; k < 5; k++) {
                 var newInput = $("<input>");
                 newInput.attr({ type: "radio", name: "question", class: "option", data: String.fromCharCode(k + 64) });
                 console.log(String.fromCharCode(k + 64));
                 $(".choice" + k).append(newInput);
             }
-            questionDiv.append("<div id='questionsDom" + (i + 1) + "'>");
+            
         }
     }
     generateQuestions();
