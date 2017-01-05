cc7e6b80610637866494cbc50407f5c097f7c97e
James <jwin4740@gmail.com>
2017-01-05 10:58:38 -0500

    dog

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,46 @@
 $(document).ready(function() {
-    var questionDiv = "";
+            var questionDiv = "";
 
-    $(".optiona").append("hello world");
+            $(".optiona").append("hello world");
 
 
-    // function that creates the question then inserts it into the DOM
+            // function that creates the question then inserts it into the DOM
 
-    function generateQuestions() {
+            function generateQuestions() {
+                function makeForm() {
+                    for (var i = 1; i <= 2; i++) {
+                        questionDiv = $("#questions");
+                        var newForm = $("<form>");
+                        newForm.attr({id: "question" + i, class: "domanda"});
+                        questionDiv.append(newForm);
 
-        for (var i = 1; i <= 1; i++) {
-            questionDiv = $("#questionsDom");
-            var newForm = $("<form>");
-            newForm.attr("id", "question" + i);
-            questionDiv.append(newForm);
-            for (var j = 1; j < 5; j++) {
-                var newDiv = $("<div class='choice" + j + " pick'>");
-                $("#question" + i).append(newDiv);
+                        for (var j = 1; j < 5; j++) {
+                            var newDiv = $("<div class='choice" + j + " pick'>");
+                            $("#question" + i).append(newDiv);
+                        }
+
+                    }
+
+                }
+                makeForm();
+
+                function addChoices () {
+                	  for (var k = 1; k < 5; k++) {
+                    var newInput = $("<input>");
+                    newInput.attr({ type: "radio", name: "question", class: "option", data: String.fromCharCode(k + 64) });
+                    console.log(String.fromCharCode(k + 64));
+                    $(".choice" + k).append(newInput);
+                }
+
+                }
+            	addChoices();
             }
-            for (var k = 1; k < 5; k++) {
-                var newInput = $("<input>");
-                newInput.attr({ type: "radio", name: "question", class: "option", data: String.fromCharCode(k + 64) });
-                console.log(String.fromCharCode(k + 64));
-                $(".choice" + k).append(newInput);
-            }
-            
-        }
-    }
-    generateQuestions();
-
-
-    $(document).on("click", ".option", function() {
-        var answer = $("input[name='question']:checked").data("value");
-        console.log(answer);
-    });
+                generateQuestions();
+              
+                $(document).on("click", ".option", function() {
+                    var answer = $("input[name='question']:checked").data("value");
+                    console.log(answer);
+                });
 
 
 
@@ -46,4 +54,4 @@ $(document).ready(function() {
 
 
 
-});
+            });
